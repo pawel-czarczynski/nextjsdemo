183efb989e97f29a62b8c00f47f7e861c23481ac
Pawel Czarczynski <pawel.czarczynski@live.co.uk>
2023-05-04 19:42:33 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home(){
     </Head>
     <div className="max-w-4xl mx-auto p-5">
       <ContactForm />
-      <Link href='/pokemons'>ENTER</Link>
+      <Link className='text-white' href='/pokemons'>ENTER</Link>
     </div>
     </>
   )
